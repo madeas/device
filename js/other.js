cd283b3b3163da804f303da2991b38343c023c1b
Andrej Sharapov <an.sharapov90@gmail.com>
2018-08-09 09:27:27 +0300

    .js

--- js/other.js
+++ js/other.js
@@ -13,22 +13,23 @@ $(function() {
   $('#dragdisplay').draggable()
   $('#dragposition').draggable();
   $('#dragblock').draggable();
+  $('#dragbuttons').draggable();
+  $('#dragcreate').draggable();
+  $('#drag_back_set').draggable();
 });
 
 //добавление стилей из data-stl и выведение в поле #form_code
 $('.btn').click(function() {
   $('#bSm').css($(this).data('stl'));
-
   var newStyles = $('#bSm').attr('style').replace(/; /g, ';\n');
   var val = $('#form_code').val();
-
   $('#form_code').val(val.replace(/#bSd \{([\s\S]*?)\}/, `#bSd {\n${newStyles}\n}`));
 });
 
 // type='color'
 document.getElementById('bg_choose').onchange = e => {
   document.getElementById('choose').textContent = e.target.value;
-  document.getElementById('bSm').style.backgroundColor = e.target.value;
+  document.getElementById('bSm').style.background = e.target.value;
   let ecls = document.querySelector('#bg_choose');
   ecls.dataset.cls = e.target.value;
 }
@@ -96,6 +97,15 @@ $(function() {
   });
 });
 
+// добавление классов в id='back'
+$(function() {
+  var b = $('.btn_back_cls');
+  b.click(function() {
+    var cls = $(this).data("cls");
+    $('#back').toggleClass(cls);
+  });
+});
+
 // сохранение кода в буфер обмена
 var button = document.querySelector('.copy_code');
 button.addEventListener('click', function(event) {
@@ -116,16 +126,17 @@ function autosize() {
 }
 
 // добавление стикера на заднюю крышку
-var back = document.querySelector("#back"),
-  x = document.querySelector("#posX"),
+var back = document.querySelector('.back_cover'),
+  x = document.querySelector('#posX'),
   y = document.querySelector('#posY'),
   so = document.querySelector('#sizeOne'),
   st = document.querySelector('#sizeTwo'),
   si = document.querySelector('#sizeIn');
 (style = () => {
-  back.style.background = 'url(' + document.querySelector("#bgImage").value + ') ' + document.querySelector('[name="repeat"]:checked').parentNode.textContent + ' ' + x.value + '% ' + y.value + '%';
-  // back.style.background = `url(${document.querySelector("#bgImage").value}) black ${document.querySelector('[name="repeat"]:checked').parentNode.textContent} ${x.value}% ${y.value}%`;
+  back.style.backgroundImage = 'url(' + document.querySelector('#bgImage').value + ')';
   back.style.backgroundSize = so.checked ? 'cover' : st.checked ? 'contain' : si.value + 'px';
+  back.style.backgroundRepeat = document.querySelector('[name="repeat"]:checked').parentNode.textContent;
+  back.style.backgroundPosition = x.value + '%' + y.value + '%';
 })();
 document.querySelectorAll('input').forEach(el => el.oninput = style);
 
@@ -144,4 +155,47 @@ document.getElementById('reset').addEventListener('click', function(e) {
     el.checked = false;
   });
   bSm.style = '';
+});
+
+// добавить/убрать кнопки menu и back
+var bi = document.getElementById('binone');
+var bb = document.getElementById('back_button');
+var mb = document.getElementById('menu_button');
+bi.addEventListener('change', function() {
+  if (this.checked) {
+    bb.style.display = 'block';
+    mb.style.display = 'block';
+  } else {
+    bb.style.display = 'none';
+    mb.style.display = 'none';
+  }
+});
+
+// открыть кнопки разработчика
+var odb = document.querySelector('.opendev');
+odb.onclick = e => {
+  e.target.parentNode.style.display = 'none';
+  var devb = document.querySelectorAll('.devbutton');
+  document.querySelectorAll('.devbutton').forEach(n => n.classList.remove('devbutton'));
+};
+if (localStorage.getItem('an') == 1) opb.click();
+
+// вращение устройства
+var state = true;
+document.querySelector('.anistop').onclick = () => {
+  document.querySelector('.bsdisplay').style.animationPlayState = state ? 'running' : 'paused';
+  state = !state;
+};
+
+// настройка камеры iSight
+function toggle() {
+  var dbblock = document.getElementById('drag_back_set');
+  if (devplay.checked)
+    dbblock.style.display = 'block';
+  else
+    dbblock.style.display = 'none'
+}
+var devplay = document.getElementById('device_play');
+devplay.addEventListener('change', function() {
+  toggle();
 });
